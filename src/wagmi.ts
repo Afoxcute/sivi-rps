e5a005e251b83fc8c449deabff98b584fe8890a2
Afoxcute <jwavolabilove16@gmail.com>
2024-11-29 20:32:58 +0000

    chain addup

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,6 +1,6 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import {
-  taikoHekla,
+  lineaSepolia,
 } from "wagmi/chains";
 import { http } from 'wagmi';
 // import { taikoHekla as taikotemp } from "viem/_types/chains";
@@ -10,14 +10,14 @@ export const config = getDefaultConfig({
   appName: 'RockPaperScissors',
   projectId: '6ff8eb59587cd5a38c24cc85d30763ea',
   chains: [
-    taikoHekla,
+    lineaSepolia,
     // taikotemp
     // ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
   ],
   ssr: true,
   transports: {
-    [taikoHekla.id]: http(
-      'https://rpc.hekla.taiko.xyz'
+    [lineaSepolia.id]: http(
+      'https://rpc.sepolia.linea.build/'
     ),
   },
 });
